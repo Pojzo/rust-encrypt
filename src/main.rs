3122418304c568a109fdc1326ec2e3a700b21314
Peter Kovac <pojzinko8@gmail.com>
2024-09-17 21:01:44 +0200

    Added option to compress and decompress data when encrypting and decrypting

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::error::Error;
 use std::fs::File;
 use std::io::{self, Read, Write};
 use std::process;
@@ -30,7 +31,7 @@ fn read_file_as_bytes(path: &str) -> io::Result<Vec<u8>> {
     Ok(buffer)
 }
 
-fn decrypt_file(args: &Args, decompress: bool) {
+fn decrypt_file(args: &Args, decompress: bool) -> Result<(), Box<dyn Error>> {
     let source_file = &args.source_file;
     let file_content;
     let result = read_file_as_bytes(source_file);
@@ -43,22 +44,32 @@ fn decrypt_file(args: &Args, decompress: bool) {
             process::exit(1)
         }
     }
-    let decrypted_data = decrypt_data(&file_content);
-    match decrypted_data {
-        Ok(data) => {
-            if let Err(e) = write_bytes_to_file(&args.output_file, &data) {
-                println!("Failed to write to file: {}", e);
+    let mut data = decrypt_data(&file_content);
+    if let Err(e) = data {
+        eprintln!("Error decrypting data: {}", e);
+        process::exit(1);
+    }
+    if decompress {
+        match crypto::decompress_data(&data.unwrap()) {
+            Ok(decompressed_data) => {
+                data = Ok(decompressed_data);
+            }
+            Err(e) => {
+                eprintln!("Error decompressing data: {}", e);
+                process::exit(1);
             }
         }
-        Err(_e) => {
-            println!("Error");
-        }
+    }
+
+    match write_bytes_to_file(&args.output_file, &data.unwrap()) {
+        Ok(result) => Ok(()),
+        Err(e) => Err(Box::new(e)),
     }
 }
 
-fn encrypt_file(args: &Args, compress: bool) {
+fn encrypt_file(args: &Args, compress: bool) -> Result<(), Box<dyn Error>> {
     let source_file = &args.source_file;
-    let file_content;
+    let mut file_content;
     let result = read_file_as_bytes(source_file);
     match result {
         Ok(contents) => {
@@ -69,6 +80,20 @@ fn encrypt_file(args: &Args, compress: bool) {
             process::exit(1)
         }
     }
+
+    if compress {
+        println!("compressing data");
+        match crypto::compress_data(&file_content) {
+            Ok(compressed_data) => {
+                file_content = compressed_data;
+            }
+            Err(e) => {
+                eprintln!("Error compressing data: {}", e);
+                process::exit(1);
+            }
+        }
+    }
+
     let original_size = file_content.len() as u32;
 
     let mut size_buffer = Vec::new();
@@ -80,8 +105,11 @@ fn encrypt_file(args: &Args, compress: bool) {
     let encrypted_data = encrypt_data(&file_content);
 
     match write_bytes_to_file(&args.output_file, &encrypted_data) {
-        Ok(_result) => {}
-        Err(_e) => {}
+        Ok(_result) => Ok(()),
+        Err(_e) => Err(Box::new(io::Error::new(
+            io::ErrorKind::Other,
+            "Failed to write to file",
+        ))),
     }
 }
 
